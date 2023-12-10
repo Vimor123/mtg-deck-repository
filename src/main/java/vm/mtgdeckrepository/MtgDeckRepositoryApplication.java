07583ddf4d77fde916893402c6b0cf2b3f39a975
Vimor123 <vmladar1@gmail.com>
2023-12-10 20:57:16 +0100

    Project initialized

---
+++ src/main/java/vm/mtgdeckrepository/MtgDeckRepositoryApplication.java
@@ -0,0 +1,13 @@
+package vm.mtgdeckrepository;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MtgDeckRepositoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MtgDeckRepositoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vm/mtgdeckrepository/MtgDeckRepositoryApplicationTests.java
@@ -0,0 +1,13 @@
+package vm.mtgdeckrepository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MtgDeckRepositoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
